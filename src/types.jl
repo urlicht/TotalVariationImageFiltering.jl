a6d375768212172e50111ead85b9e4e619b50995
urlicht <urlicht@users.noreply.github.com>
2026-03-18 09:50:26 +0000

    Add AbstractTVSolver, SolverStats, _validate_common_config

--- src/types.jl
+++ src/types.jl
@@ -1,3 +1,7 @@
+#
+# Boundary condition
+#
+
 """
 Abstract supertype for boundary conventions used by differential operators.
 """
@@ -8,6 +12,10 @@ Forward-difference gradient with homogeneous Neumann boundary treatment.
 """
 struct Neumann <: AbstractBoundaryCondition end
 
+#
+# Data fidelity
+#
+
 """
 Abstract supertype for data-fidelity terms.
 """
@@ -17,6 +25,10 @@ abstract type AbstractDataFidelity end
 L2 data fidelity, `0.5 * ||u - f||^2`.
 """
 struct L2Fidelity <: AbstractDataFidelity end
+
+#
+# TVMode (pixel/voxel) isotropy
+#
 
 """
 Abstract supertype for TV penalty modes.
@@ -33,3 +45,26 @@ Anisotropic TV, `sum(sum_d |grad_d u|)`.
 """
 struct AnisotropicTV <: AbstractTVMode end
 
+#
+# Solver
+#
+"""
+Abstract supertype for all total-variation solver configurations.
+"""
+abstract type AbstractTVSolver end
+
+"""
+Convergence summary for one solver run.
+"""
+struct SolverStats{T<:AbstractFloat}
+    iterations::Int
+    converged::Bool
+    rel_change::T
+end
+
+function _validate_common_config(maxiter::Int, check_every::Int)
+    maxiter > 0 || throw(ArgumentError("maxiter must be positive, got $maxiter"))
+    check_every > 0 ||
+        throw(ArgumentError("check_every must be positive, got $check_every"))
+    return nothing
+end
